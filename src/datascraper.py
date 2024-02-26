707f1579218958fb4fa3dd8c5c6e41f074bd23e9
saehejkang <saehej.kang@gmail.com>
2024-02-25 22:41:50 -0800

    read/write to csv + csv file updated

--- src/datascraper.py
+++ src/datascraper.py
@@ -1,8 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
 
 
-def read_from_url(url):
+def read_from_url(url, college, college_id):
+    # set the init values of the dictionary
+    finalData = {}
+    finalData['College'] = college
+    finalData['Id'] = college_id
+
+    # http request for site
     session = requests.Session()
     response = session.get(url, headers={'User-Agent': 'Mozilla/5.0'})
 
@@ -20,11 +27,36 @@ def read_from_url(url):
         # find the value needed in the corresponding tag
         value = rank.findNext("td", align="right")
         # output the stat name
-        print(stat.text)
+        stat = stat.text
         # output the stat value
-        print(value.text.strip())
+        val = value.text.strip()
+        finalData[stat] = val
+    return finalData
 
 
-def write_to_csv():
-    # TODO
-    return
+def run_datascraper():
+    fields = ["College", "Id", "Total Offense", "Rushing Offense", "Passing Offense",
+              "Team Passing Efficiency", "Scoring Offense", "Total Defense", "Rushing Defense",
+              "Passing Yards Allowed", "Team Passing Efficiency Defense", "Scoring Defense",
+              "Turnover Margin", "3rd Down Conversion Pct", "4th Down Conversion Pct", "3rd Down Conversion Pct Defense",
+              "4th Down Conversion Pct Defense", "Red Zone Offense", "Red Zone Defense", "Net Punting", "Punt Returns",
+              "Kickoff Returns", "First Downs Offense", "First Downs Defense", "Fewest Penalties Per Game",
+              "Fewest Penalty Yards Per Game", "Time of Possession"]
+    dict_to_write = []
+    fileName = '../data/cfb_teams_to_id.csv'
+
+    with open(fileName, newline='', mode='r') as csvfile:
+        college_data = csv.reader(csvfile, delimiter=',', quotechar='|')
+        for college in college_data:
+            # skip the first row of the csv file
+            if college[1] != "Id":
+                url = "https://stats.ncaa.org/teams/{id}".format(id=college[1])
+                data = read_from_url(url, college[0], college[1])
+                dict_to_write.append(data)
+        with open(fileName, 'w') as csvfile:
+            # creating a csv dict writer object
+            writer = csv.DictWriter(csvfile, fieldnames=fields)
+            # write the fields to the file
+            writer.writeheader()
+            # writing data rows
+            writer.writerows(dict_to_write)
--- src/main.py
+++ src/main.py
@@ -1,7 +1,5 @@
 import datascraper
 
-# TODO add dictionary of teams so that we can iterate through all of them in one go
-
 if __name__ == '__main__':
-    datascraper.read_from_url("https://stats.ncaa.org/teams/557270")
-
+    # run the datascraper to collect data
+    college_data = datascraper.run_datascraper()
